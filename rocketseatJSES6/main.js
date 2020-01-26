d6a455ac238de0fe8a70aa227b7780f4fe563116
victorgcampanella <vgcgalvao@outlook.com>
2020-01-26 14:36:29 -0300

    class em JS

--- rocketseatJSES6/main.js
+++ rocketseatJSES6/main.js
@@ -1,8 +1,39 @@
-class Teste{
-  metodo(){
-
+class List{
+  constructor(){
+    this.data = []
   }
-  outro(){
 
+  add(data){
+    this.data.push(data)
+    console.log(this.data)
   }
 }
+
+class TodoList extends List{
+  constructor(){
+    super()
+
+    this.usuario = 'Diego'
+    }
+
+    mostraUsuario(){
+      console.log(this.usuario)
+  }
+
+  // constructor(){
+  //   this.todos = []
+  // }
+
+  // addTodo(){
+  //   this.todos.push('Novo Todo')
+  //   console.log(this.todos)
+  // }
+}
+
+var MinhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function(){
+  MinhaLista.add('Novo todo')
+}
+
+MinhaLista.mostraUsuario()
