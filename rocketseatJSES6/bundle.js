0d59b9addfca7ee179fd7c17e454a196c387752d
victorgcampanella <vgcgalvao@outlook.com>
2020-01-26 14:43:14 -0300

    usando const e let

--- rocketseatJSES6/bundle.js
+++ rocketseatJSES6/bundle.js
@@ -1,26 +1,17 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/*const usuario = {nome: 'Diego'}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+usuario.nome = 'Cleiton'
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+console.log(usuario)*/
+function teste(x) {
+  var y = 2;
 
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
+  if (x > 5) {
+    var _y = 4;
+    console.log(x, _y);
   }
+}
 
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 4));
+teste(10);
---
+++ rocketseatJSES6/main2.js
@@ -0,0 +1,17 @@
+/*const usuario = {nome: 'Diego'}
+
+usuario.nome = 'Cleiton'
+
+console.log(usuario)*/
+
+function teste(x){
+  let y = 2
+
+  if(x > 5){
+    let y = 4
+    console.log(x, y)
+  }
+
+}
+
+teste(10)
