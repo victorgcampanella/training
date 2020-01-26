e55fbe94d057e6cacf5c54168b533c71926a4ef6
victorgcampanella <vgcgalvao@outlook.com>
2020-01-25 21:55:28 -0300

    usando axios

--- rocketseatJS/jsassincrono/main.js
+++ rocketseatJS/jsassincrono/main.js
@@ -1,10 +1,44 @@
-var xhr = new XMLHttpRequest()
+// var xhr = new XMLHttpRequest()
 
-xhr.open('GET', 'https://api.github.com./users/diego3g')
-xhr.send(null)
+// xhr.open('GET', 'https://api.github.com./users/diego3g')
+// xhr.send(null)
 
-xhr.onreadystatechange = function(){
-  if(xhr.readyState == 4){
-    console.log(JSON.parse(xhr.response))
-  }
+// xhr.onreadystatechange = function(){
+//   if(xhr.readyState == 4){
+//     console.log(JSON.parse(xhr.responseText))
+//   }
+// }
+
+/*var minhaPromise = function(){
+  return new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://api.github.com./users/diego3g')
+    xhr.send(null)
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          if(xhr.status == 200){
+            resolve(JSON.parse(xhr.responseText))
+          }else{
+            reject('Erro na requisicao')
+          }
+        }
+      }
+  })
 }
+
+minhaPromise()
+.then(function(response){
+  console.log(response)
+})
+.catch(function(error){
+  console.log(error)
+})*/
+
+axios.get('https://api.github.com./users/diego3g')
+.then(function(response){
+  console.log(response.data.login)
+})
+.catch(function(error){
+  console.log(error)
+})
