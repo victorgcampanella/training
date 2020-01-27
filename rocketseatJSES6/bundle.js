c1a7b43714f4f5843b021e540f7c1d0a0b4a8ad2
victorgcampanella <vgcgalvao@outlook.com>
2020-01-27 17:28:16 -0300

    operacoes em array

--- rocketseatJSES6/bundle.js
+++ rocketseatJSES6/bundle.js
@@ -1,17 +1,22 @@
 "use strict";
 
-/*const usuario = {nome: 'Diego'}
-
-usuario.nome = 'Cleiton'
-
-console.log(usuario)*/
-function teste(x) {
-  var y = 2;
-
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
-}
-
-teste(10);
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return (
+    /*item * 2,*/
+    item + index
+  );
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 == 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item == 4;
+});
+console.log(find);
