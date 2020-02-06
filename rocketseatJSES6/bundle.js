decfe12014b64b0764ae3787de370e0b00eb4d89
Victor Campanella <victor.campanella@itst.com.br>
2020-02-06 14:21:31 -0300

    deletando arquivo de readme

--- rocketseatJSES6/bundle.js
+++ rocketseatJSES6/bundle.js
@@ -4,7 +4,7 @@
   return a + b
 }*/
 var soma = function soma() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 4;
   var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
   return a + b;
 };
--- rocketseatJSES6/main5.js
+++ rocketseatJSES6/main5.js
@@ -1,7 +1,7 @@
 /*function soma(a = 3, b = 6){
   return a + b
 }*/
-const soma = (a = 3, b = 6) => a + b
+const soma = (a = 4, b = 6) => a + b
 
 console.log(soma(1))
 console.log(soma())
